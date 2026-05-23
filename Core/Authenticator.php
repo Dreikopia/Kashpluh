4b5a6f3e1e625901f1bf82bd56daf3d620b2005f
Dreikopia <andreiheatherquintos@gmail.com>
2026-05-23 09:26:24 +0800

    added a method for registration

--- Core/Authenticator.php
+++ Core/Authenticator.php
@@ -7,27 +7,59 @@ use Core\Database;
 
 class Authenticator
 {
-    public function attempt($login, $password)
+    public Database $db;
+
+    public function __construct()
     {
-        App::resolve(Database::class);
+        $this->db = App::resolve(Database::class);
+    }
 
-        $query = "SELECT * FROM users WHERE email = :email OR username = :username";
+    public function attemptToRegister($username, $email, $password)
+    {
+        $this->db->query("INSERT INTO users(username, email, pwd)
+        VALUES(:username, :email, :pwd)", [
+            ':username' => $username,
+            ':email' => $email,
+            ':pwd' => password_hash($password, PASSWORD_BCRYPT)
+        ]);
+        $this->login([
+            'username' => $username,
+            'email' => $email
+        ]);
+    }
 
-        $user = App::resolve(Database::class)->query($query, [
-            ':username' => $login,
-            ':email' => $login,
-        ])->find();
+    public function attemptToLogin($login, $password)
+    {
+        $user = $this->db
+            ->query("SELECT * FROM users WHERE email = :email OR username = :username", [
+                ':username' => $login,
+                ':email'    => $login,
+            ])
+            ->find();
 
-        if ($user) {
-            if (password_verify($password, $user['pwd'])) {
-
-                login([
-                    'login' => $login
-                ]);
-
-                return true;
-            }
+        if ($user && password_verify($password, $user['pwd'])) {
+            $this->login(['login' => $login]);
+            return true;
         }
+
         return false;
+    }
+
+
+
+    public function login($user)
+    {
+        $_SESSION['user'] = [
+            'login'    => $user['login'] ?? null,
+            'username' => $user['username'] ?? null,
+            'email'    => $user['email'] ?? null,
+        ];
+        session_regenerate_id(true);
+    }
+
+    public function logout()
+    {
+
+        Session::destroy();
     }
 }
